4825ddc0ea330e1aa47aacd2c9e58cf3f430225e
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 10:35:27 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
-
+ if block_given?
+   i=0;
+   block_return_values =[]
+   while i<collection.length;
+   block_return_values << yield(collection[i])
+   i+=1;
+   end
+ else "No block given"
+ end
+ 
+ if block_return_values.include?(false)
+   return false;
+ end
+ true
 end
