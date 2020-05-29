93c7de3cf931f59be7125e4bced79dfa9d92b05a
seanbalke <sean.balke@gmail.com>
2020-05-29 17:19:10 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
-def my_all?(collection)
+# YIELDS CORRECT ELEMENT TO BLOCK ([1, 2, 3]) W/ SPACES
+# RETURNS FALSE WHEN CONDITION NOT MET
+# RETURNS TRUE WHEN CONDITION IS MET
 
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
+
